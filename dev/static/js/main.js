2e6de0f0506c3f359dbea78c27ee8ed0379156d8
ewgenij87sn <ewgenij87sn@gmail.com>
2018-04-07 21:15:53 +0200

    2 lessons

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
     svg4everybody({});
 });
+
+$(document).ready(function() {
+	$('.sandwich').click(function() {
+		var menu = $('.header-nav__list');
+		$(this).toggleClass('active');
+		if (menu.is(':visible')) {
+			menu.slideUp();
+		} else {
+			menu.slideDown();
+		}
+
+	})
+})
